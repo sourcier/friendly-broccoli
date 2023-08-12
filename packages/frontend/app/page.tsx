a6808821be4111fbf3cbbb0faed38d00e2f79898
Roger Rajaratnam <16674724+sourcier@users.noreply.github.com>
2023-08-12 16:53:17 +0100

    Add hero component (#18)
    * create hero component
    
    * add props to hero

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,1 +1,9 @@
-export default function Page() {}
+import config from '../config';
+import { Hero } from '../components/server/hero';
+
+export default function Page() {
+  const { hero } = config.homepage;
+  return (
+    <Hero title={hero.title} subTitle={hero.subTitle} image={hero.image} />
+  );
+}
