ab49bcb3a8cae955fe17801c1c09eae6cb43d2ba
Roger Rajaratnam <16674724+sourcier@users.noreply.github.com>
2023-08-27 00:27:56 +0100

    setup custom domains (#24)

--- packages/stacks/FrontendStack.ts
+++ packages/stacks/FrontendStack.ts
@@ -1,8 +1,21 @@
 import { NextjsSite, StackContext } from 'sst/constructs';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 export default function FrontendStack({ stack }: StackContext) {
   const site = new NextjsSite(stack, 'NextJsSite', {
     path: 'packages/frontend',
+    customDomain: {
+      domainName:
+        stack.stage === 'prod' ? 'sourcier.uk' : `${stack.stage}.sourcier.uk`,
+      isExternalDomain: true,
+      cdk: {
+        certificate: Certificate.fromCertificateArn(
+          stack,
+          '91918ff7-9013-45ad-8a7b-30f3dd928efe',
+          'arn:aws:acm:eu-west-2:561381207426:certificate/91918ff7-9013-45ad-8a7b-30f3dd928efe'
+        ),
+      },
+    },
   });
 
   stack.addOutputs({
