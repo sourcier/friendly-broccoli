c18b09518eedb1e4eba169d8db7ea2ea2d1ec692
Roger Rajaratnam <roger@sourcier.uk>
2023-08-28 01:15:27 +0100

    add ui.router config to tinacms and restore postcss config

--- packages/frontend/tina/config.ts
+++ packages/frontend/tina/config.ts
@@ -90,6 +90,9 @@ const config = defineConfig({
             isBody: true,
           },
         ],
+        ui: {
+          router: async ({ document }) => `/blog/${document._sys.filename}`,
+        },
       },
       {
         name: 'homepage',
@@ -123,6 +126,7 @@ const config = defineConfig({
             create: false,
             delete: false,
           },
+          router: async () => `/`,
         },
       },
     ],
