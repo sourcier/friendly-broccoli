8d33d2b7bda958c8fd56864a21a1fc0ab4f74847
Roger Rajaratnam <roger@sourcier.uk>
2023-09-03 02:17:35 +0100

    update types

--- packages/frontend/app/[filename]/page.tsx
+++ packages/frontend/app/[filename]/page.tsx
@@ -2,11 +2,19 @@ import { fetchTinaData } from '../../lib/tinacms';
 import { Page } from '../../components/pages';
 import { client } from '../../tina/__generated__/client';
 
+type Params = {
+  filename: string;
+};
+
+type MarketingPageProps = {
+  params: Params;
+};
+
+export const dynamicParams = false;
+
 export default async function MarketingPage({
   params: { filename },
-}: {
-  params: { filename: any };
-}) {
+}: MarketingPageProps) {
   const {
     props: { data, query, variables },
   } = await fetchTinaData({
@@ -16,11 +24,13 @@ export default async function MarketingPage({
   return <Page data={data} query={query} variables={variables} />;
 }
 
-export async function generateStaticParams() {
+export async function generateStaticParams(): Promise<Params[]> {
   const pagesResponse = await client.queries.pagesConnection();
-  return pagesResponse.data.pagesConnection.edges
-    ?.filter((page) => page?.node?._sys.filename !== 'index')
-    .map((page) => ({
-      filename: page?.node?._sys.filename,
-    }));
+  return (
+    pagesResponse.data.pagesConnection.edges
+      ?.filter((page) => page!.node!._sys.filename !== 'index')
+      .map<{ filename: string }>((page) => ({
+        filename: page!.node!._sys.filename,
+      })) || []
+  );
 }
